199bab47ae656715f195081700f6dc8c1323ce71
Md Sakib Hossain <sakib.aiubedu.cse@gmail.com>
2021-02-07 16:03:55 +0600

    lab 3

---
+++ lab 3/prb2.php
@@ -0,0 +1,29 @@
+<?php
+    $marks = 85;
+
+    if ($marks >=90){
+        echo "A+";
+    }
+    elseif ($marks >=80 && $marks <90){
+        echo "A and marks: $marks";
+    }
+    elseif ($marks >=70 && $marks <80){
+        echo "B and marks: $marks";
+    }
+    elseif ($marks >=60 && $marks <70){
+        echo "C and marks: $marks";
+    } 
+    else{
+        echo "F Grade";
+    }
+    
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ lab 3/prb3.php
@@ -0,0 +1,15 @@
+<?php 
+    $lenght = 5 ;
+    $width = 6;
+    $Area = $lenght * $width ;
+    $Perimeter = 2*( $lenght + $width );
+    echo "Lenght and width: $lenght and $width <br>";
+    echo "Area: $Area <br>";
+    echo "Perimeter: $Perimeter <br>";
+    if ($lenght == $width){
+        echo "The shape is a square";
+    }
+    else{
+
+    }
+?>
